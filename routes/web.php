ea9f77d529e82e99f7e76d26f4e350d940cd267a
Katherin Sanchez <dksanchez476@misena.edu.co>
2021-04-27 09:11:52 -0500

    proyecto creado

---
+++ routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('variables' ,function(){
+    $mensaje = 20;
+
+    // funcion nativa de php: analizar el contenido de una variable
+    // variable: tipo de dato, atos contenidos.
+    var_dump($mensaje);
+    echo "<hr />";
+    $mensaje =" Hola mi gente";
+    var_dump($mensaje);
+});
+
+Route::get("arreglos" , function(){
+
+    // definir un arreglo en php
+    //tamaño: numero de elementos del arreglo
+    // el tamaño del arreglo estudiantes es 3
+    $estudiantes = [  "Ana", "Daniel",  "Brayan"];
+    echo "<pre>";
+    var_dump($estudiantes);
+    echo "</pre>";
+} );
